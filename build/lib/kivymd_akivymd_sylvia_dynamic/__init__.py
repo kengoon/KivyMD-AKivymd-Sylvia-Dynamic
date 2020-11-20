453996ae1151383eb75637fb8a6d4bcf28c71081
kengoon <kengoon19@gmail.com>
2020-11-20 17:05:31 +0100

    delete

--- build/lib/kivymd_akivymd_sylvia_dynamic/__init__.py
+++
@@ -1,5 +0,0 @@
-from kivymd_akivymd_sylvia_dynamic.tools.iconfonts import register
-
-register('fontello', 'icont/fontello.ttf', 'icont/fontello.fontd')
-register("icomoon", "icont/icomoon.ttf", "icont/icomoon.fontd")
-register("icomoon2", "icont/icomoon2.ttf", "icont/icomoon2.fontd")
