1648617c161078e51898a48b7b1035fb2f00f347
kengoon <kengoon19@gmail.com>
2020-11-21 13:57:41 +0100

    Update README.md
    Signed-off-by: kengoon <kengoon19@gmail.com>

--- kivymd_akivymd_sylvia_dynamic/__init__.py
+++ kivymd_akivymd_sylvia_dynamic/__init__.py
@@ -1,5 +1,12 @@
+import os
+
+from kivy import Logger
+
 from kivymd_akivymd_sylvia_dynamic.tools.iconfonts import register
 
-register('fontello', 'icont/fontello.ttf', 'icont/fontello.fontd')
-register("icomoon", "icont/icomoon.ttf", "icont/icomoon.fontd")
-register("icomoon2", "icont/icomoon2.ttf", "icont/icomoon2.fontd")
+path = os.path.dirname(__file__)
+fonts_path = os.path.join(path, f"icont{os.sep}")
+Logger.info("KASD: 0.1")
+register('fontello', f'{fonts_path}/fontello.ttf', f'{fonts_path}/fontello.fontd')
+register("icomoon", f"{fonts_path}/icomoon.ttf", f"{fonts_path}/icomoon.fontd")
+register("icomoon2", f"{fonts_path}/icomoon2.ttf", f"{fonts_path}/icomoon2.fontd")
